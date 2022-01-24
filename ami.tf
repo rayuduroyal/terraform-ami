8f1a990bf2a3f1cc064f85978258512774e3371c
susarlarayudu <rayuduroyal1996@gmail.com>
2022-01-24 14:32:31 +0530

    auto pushing chainges repo

--- ami.tf
+++ ami.tf
@@ -1,6 +1,7 @@
 resource "aws_ami_from_instance" "ami" {
-  name               = "${var.COMPONENT}-${var.APP_VERSION}"
-  source_instance_id = aws_instance.ec2.id
+  depends_on          = [null_resource.app-deploy]
+  name                = "${var.COMPONENT}-${var.APP_VERSION}"
+  source_instance_id  = aws_instance.ec2.id
   tags = {
     Name = "${var.COMPONENT}-${var.APP_VERSION}"
     VERSION = var.APP_VERSION
